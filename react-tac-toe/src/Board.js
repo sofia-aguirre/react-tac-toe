3c62d15c502eddd65bf8e2692c47e2fc3c520688
justincastilla <pxlperfection@gmail.com>
2018-09-27 14:54:42 -0700

    added board tracking

--- react-tac-toe/src/Board.js
+++ react-tac-toe/src/Board.js
@@ -3,20 +3,39 @@ import Square from './Square'
 import './App.css';
 
 class Board extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      player1: true,
+      game: ['', '', '', '', '', '', '', '', '']
+    }
+  }
+
+  reportMark = (pos) => {
+    console.log(pos)
+  }
+
   render() {
+
+    let squares = []
+
+    for(let i = 0; i < 9; i++){
+      squares.push( <Square 
+                      key={i} 
+                      pos={i} 
+                      mark={this.state.game[i]}
+                      reportMark={this.reportMark} // giving my function to Square
+                      /> )
+    }
+
     return (
       <div>
         <h1>React-Tac-Toe</h1>
+
+        <h4>Player: {this.state.player1 ? 'Player 1' : 'Player 2' }</h4>
         <div className="board">
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
+          {squares}
         </div>
       </div>
     );
--- react-tac-toe/src/Square.js
+++ react-tac-toe/src/Square.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Square extends Component {
+  
+  handleClick = () => {
+    // call a function from Board.js to update the game array with the mark.
+    this.props.reportMark(this.props.pos)
+  }
+
   render() {
     return (
-          <div className="square"></div>
+      <div className="square" onClick={ this.handleClick }>
+        <h1>{this.props.mark}</h1>
+      </div>
     );
   }
 }
